2e32262adc2982d4488e0798a05851893f7ec6ae
André Davis <davis.andre@gmail.com>
2019-07-21 10:24:48 -0700

    Initial commit. Getting setup to learn Selenium

---
+++ SeleniumPlayground/SeleniumPlayground/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumPlayground
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
